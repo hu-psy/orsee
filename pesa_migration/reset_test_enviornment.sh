a26d47d15e98ea20d861fabfce426c06886d19b3
root@hupsy07 <root@hupsy07.psychologie.hu-berlin.de>
2019-06-19 16:18:10 +0200

    adjust installation path for hupsy07

--- pesa_migration/reset_test_enviornment.sh
+++ pesa_migration/reset_test_enviornment.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# drop pesa2019
+mysql -e 'drop database pesa2019'
+# create pesa2019
+mysql -e 'create database pesa2019 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
 # clear pesa2019 tables
 mysql -Nse 'show tables in pesa2019' | while read table; do mysql -e "truncate table pesa2019.$table"; done
 # re-init tables
-mysql pesa2019 < /var/www/html/pesa2019/install/install.sql
+mysql pesa2019 < /var/www/html/pesa/install/install.sql
 
 # clear and load old pesa
 mysql pesa2019 -e 'drop database pesa'
