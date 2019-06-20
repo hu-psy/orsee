1d6e064fdc14e139cd6a4758ddff35aab2a113af
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-06-20 14:07:58 +0200

    set migration as default value

--- pesa_migration/pesa_set_options.sql
+++ pesa_migration/pesa_set_options.sql
@@ -2,6 +2,6 @@ update pesa2019.or_options set option_value = 'y' where (option_id = '1426010295
 update pesa2019.or_options set option_value = 'y' where (option_id = '1426336392'); /* subject cancellation */
 update pesa2019.or_options set option_value = 'y' where (option_id = '1426010293'); /* require privacy acceptance */
 update pesa2019.or_options set option_value = 'y' where (option_id = '1426010292'); /* require rules acceptance */
-/* update pesa2019.or_options set option_value = 'username_password' where (option_id = '1426010310'); /* subject authentication */
+update pesa2019.or_options set option_value = 'migration' where (option_id = '1426010310'); /* subject authentication */
 /* update pesa2019.or_options set option_value = '<insert-email-here>' where (option_id = '1426010269'); /* support mail */
 
