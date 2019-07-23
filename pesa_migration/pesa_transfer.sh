c72cc33bd3dce573f1b4619f11f740b7b4aabd53
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-07-23 11:25:59 +0200

    add to transfer script

--- pesa_migration/pesa_transfer.sh
+++ pesa_migration/pesa_transfer.sh
@@ -15,7 +15,8 @@ pesa_transfer_contact.sql
 pesa_transfer_impressum.sql
 pesa_transfer_subpools.sql
 pesa_transfer_mail.sql
-pesa_transfer_welcome.sql)
+pesa_transfer_welcome.sql
+pesa_add_mail_templates.sql)
 
 # transfering or_admin, this requires a .htaccess file
 ./pesa_transfer_admin.sh
