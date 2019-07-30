0a06e84d867330bf47814542a893632c253f9167
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-07-30 14:55:09 +0200

    forgot the answers to the faq

--- pesa_migration/pesa_transfer_faqs.sql
+++ pesa_migration/pesa_transfer_faqs.sql
@@ -2,7 +2,8 @@ truncate table ##new_db##.or_faqs;
 insert into ##new_db##.or_faqs(faq_id, evaluation) 
 select faq_id, evaluation from ##old_db##.or_faqs;
 
-update ##old_db##.or_lang set lang_id = lang_id + 220004 where content_type = "faq_question";
+update ##old_db##.or_lang set lang_id = lang_id + 220004 where content_type = "faq_question" or content_type = "faq_answer";
+
 insert into ##new_db##.or_lang(lang_id, enabled, content_type, content_name, en, de) 
-select lang_id, enabled, content_type, content_name, en, de from ##old_db##.or_lang where content_type = "faq_question";
+select lang_id, enabled, content_type, content_name, en, de from ##old_db##.or_lang where content_type = "faq_question" or content_type = "faq_answer";
 
