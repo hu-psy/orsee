4f7162d47ef71ed7e6293e06e5f76eafd056fcd8
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-09-19 18:26:51 +0200

    disable ethics module by default

--- pesa_migration/pesa_set_options.sql
+++ pesa_migration/pesa_set_options.sql
@@ -3,5 +3,6 @@ update ##new_db##.or_options set option_value = 'y' where (option_id = '14263363
 update ##new_db##.or_options set option_value = 'y' where (option_id = '1426010293'); /* require privacy acceptance */
 update ##new_db##.or_options set option_value = 'y' where (option_id = '1426010292'); /* require rules acceptance */
 update ##new_db##.or_options set option_value = 'migration' where (option_id = '1426010310'); /* subject authentication */
+update ##new_db##.or_options set option_value = 'n' where (option_id = '1426010286'); /* disable ethics module */
 /* update pesa2019.or_options set option_value = '<insert-email-here>' where (option_id = '1426010269'); /* support mail */
 
