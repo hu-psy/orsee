f619fca10c7e435e311d841b808bb91c413e574f
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-09-20 12:38:26 +0200

    enable no show warnings per email

--- pesa_migration/pesa_set_options.sql
+++ pesa_migration/pesa_set_options.sql
@@ -5,3 +5,6 @@ update ##new_db##.or_options set option_value = 'y' where (option_id = '14260102
 update ##new_db##.or_options set option_value = 'migration' where (option_id = '1426010310'); /* subject authentication */
 /* update pesa2019.or_options set option_value = '<insert-email-here>' where (option_id = '1426010269'); /* support mail */
 
+/* enable noshow warnings */
+update ##new_db##.or_options set option_value = 'y' where option_id='1426010300';
+update ##new_db##.or_cron_jobs set enabled = 'y' where job_name="check_for_noshow_warnings";
