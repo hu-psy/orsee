61c3295c1960bf2dab9862e098e57b2f4f80209f
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-10-02 15:23:12 +0200

    disallow experimenter to see and modify participants (in none experiment context)

--- pesa_migration/pesa_set_rights.sql
+++ pesa_migration/pesa_set_rights.sql
@@ -1,2 +1,9 @@
+/* remove rights */
+update ##new_db##.or_admin_types set rights=replace(rights, ",participants_bulk_mail", "") where type_name="experimenter";
+update ##new_db##.or_admin_types set rights=replace(rights, ",participants_edit", "") where type_name="experimenter";
+update ##new_db##.or_admin_types set rights=replace(rights, ",participants_change_status", "") where type_name="experimenter";
+update ##new_db##.or_admin_types set rights=replace(rights, ",participants_show", "") where type_name="experimenter";
 update ##new_db##.or_admin_types set rights=replace(rights, ",experiment_restriction_override", "") where type_name="experimenter";
+
+/* add rights */
 update ##new_db##.or_admin_types set rights=concat(rights, ",experiment_change_sender_address") where type_name="experimenter";
