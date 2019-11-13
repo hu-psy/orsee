224a3b15c2f92142961d286200bba7b1f8c4c870
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-11-13 19:32:45 +0100

    adapt reset_test_enviornment.sh

--- pesa_migration/reset_test_enviornment.sh
+++ pesa_migration/reset_test_enviornment.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # drop pesa2019
-mysql -e 'drop database pesa2019'
+# mysql -e 'drop database pesa'
 # create pesa2019
-mysql -e 'create database pesa2019 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
+# mysql -e 'create database pesa DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
 # clear pesa2019 tables
-mysql -Nse 'show tables in pesa2019' | while read table; do mysql -e "truncate table pesa2019.$table"; done
+mysql -Nse 'show tables in pesa' | while read table; do mysql -e "truncate table pesa.$table"; done
 # re-init tables
-mysql pesa2019 < /var/www/html/pesa/install/install.sql
+mysql pesa < ../install/install.sql
 
 # clear and load old pesa
-mysql pesa2019 -e 'drop database pesa'
-mysql < pesa.20190613.sql
+mysql pesa -e 'drop database oldpesa'
+mysql < pesa.20190402.sql
 
