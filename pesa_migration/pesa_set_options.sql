5ee3ce0966a07852e1404b9932bb8e62297d4030
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-11-28 18:01:53 +0100

    set max session duration and default values for participants and reserve participants

--- pesa_migration/pesa_set_options.sql
+++ pesa_migration/pesa_set_options.sql
@@ -6,6 +6,9 @@ update ##new_db##.or_options set option_value = 'migration' where (option_id = '
 update ##new_db##.or_options set option_value = 'n' where (option_id = '1426010286'); /* disable ethics module */
 update ##new_db##.or_options set option_value = 'y' where (option_id = '1426010298'); /* allow modification of reminder mails */
 update ##new_db##.or_options set option_value = 'y' where (option_id = '1426010305'); /* allow modification of sender mail */
+update ##new_db##.or_options set option_value = 10  where (option_id = '1426012101'); /* set max session duration hour default */
+update ##new_db##.or_options set option_value = 0   where (option_id = '1426012100'); /* set reserve participant default */
+update ##new_db##.or_options set option_value = 1   where (option_id = '1426012098'); /* set participant default */
 /* update pesa2019.or_options set option_value = '<insert-email-here>' where (option_id = '1426010269'); /* support mail */
 
 /* enable noshow warnings */
