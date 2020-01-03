c2767d0c4f7204d44af2ab07c6aa6a2443d0cacb
Steffen Kaden <kaden@informatik.hu-berlin.de>
2020-01-03 18:38:13 +0100

    use session specific cancellation deadline for deciding if canccellation is possible

--- tagsets/sessions.php
+++ tagsets/sessions.php
@@ -308,10 +308,7 @@ function sessions__get_cancellation_deadline($alist,$session_id="",$experiment_i
             LIMIT 1";
         $alist=orsee_query($query,$pars);
     }
-    if (isset($settings['subject_cancellation_hours_before_start']) && $settings['subject_cancellation_hours_before_start']>0)
-        $deadline_hours=$settings['subject_cancellation_hours_before_start'];
-    else $deadline_hours=0;
-    $cancellation__deadline=ortime__add_hourmin_to_sesstime($alist['session_start'],0-$deadline_hours);
+    $cancellation__deadline=ortime__add_hourmin_to_sesstime($alist['session_start'], 0-$alist['cancellation_end_hours']);
     return ortime__sesstime_to_unixtime($cancellation__deadline);
 }
 
