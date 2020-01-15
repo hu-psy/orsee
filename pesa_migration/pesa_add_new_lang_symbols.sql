29d41e0afdeab1d2e5375709e3b024580d60be2d
Steffen Kaden <kaden@informatik.hu-berlin.de>
2020-01-15 09:58:53 +0100

    fix missing lines

--- pesa_migration/pesa_add_new_lang_symbols.sql
+++ pesa_migration/pesa_add_new_lang_symbols.sql
@@ -44,12 +44,12 @@ insert into ##new_db##.or_lang
 select max(lang_id)+1 ,'y', -1, 'lang', 'you_have_to_year_of_birth', 'Please enter your year of birth.', 'Bitte geben Sie Ihr Geburtsjahr an.' from ##new_db##.or_lang;
 
 insert into ##new_db##.or_lang
-
 select max(lang_id)+1 ,'y', -1, 'lang', 'def_expmail_subject_example', 'e.g. "Participants wanted: born after 2000, right-handed"', 'z.B. "Teilnehmer gesucht: nach 2000 geboren, Rechtshänder"' from ##new_db##.or_lang;
 
 insert into ##new_db##.or_lang
 select max(lang_id)+1 ,'y', -1, 'lang', 'error_empty_mail_parts', 'Error: please provide at least one subject and message body for at least one language.', 'Fehler: Bitte geben Sie mindestens einen Betreff und Nachrichtentext in mindestens einer Sprache an.' from ##new_db##.or_lang;
 
+insert into ##new_db##.or_lang
 select max(lang_id)+1 ,'y', -1, 'lang', 'query_experimenters_by_group', 'Prior participation - experimenter by group', 'Vorherige Teilnahme - Experimentator (gruppiert nach Gruppen)' from ##new_db##.or_lang;
 
 insert into ##new_db##.or_lang
