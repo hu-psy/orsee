e94395224284c3082cc889a70d45b15187cd0cf6
Steffen Kaden <kaden@informatik.hu-berlin.de>
2020-01-30 14:47:48 +0100

    extend admin table to store expiration date of a account

--- pesa_migration/pesa_extend_admin.sql
+++ pesa_migration/pesa_extend_admin.sql
@@ -1,2 +1,4 @@
 alter table ##new_db##.or_admin add privacy_policy_accepted char(1);
 update ##new_db##.or_admin set privacy_policy_accepted='n';
+
+alter table ##new_db##.or_admin add column expiration_date datetime default date_add(now(), interval 1 year);
