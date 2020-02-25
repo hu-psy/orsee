2e6e45d7c4d86a765bbbc761d21f2d11a8dc080a
Tiziano Zito <opossumnano@gmail.com>
2020-02-25 12:32:14 +0100

    quoting hell

--- pesa_migration/pesa_init.sh
+++ pesa_migration/pesa_init.sh
@@ -27,9 +27,9 @@ perl -i -p -e 's/settings\_\_root\_to\_server=\"\/srv\/www\/htdocs/settings\_\_r
 perl -i -p -e 's/settings\_\_root\_directory=\"\/orsee/settings\_\_root\_directory=\"\/pesa/' ../config/settings.php
 perl -i -p -e 's/settings\_\_server\_url=\"127\.0\.0\.1/settings\_\_server\_url=\"141\.20\.68\.17/' ../config/settings.php
 perl -i -p -e 's/settings\_\_server\_protocol=\"http/settings\_\_server\_protocol=\"https/' ../config/settings.php 
-perl -i -p -e 's/site\_\_database\_database=\"orsee\_db/site\_\_database\_database=\"''$DBNAME''/' ../config/settings.php
-perl -i -p -e 's/site\_\_database\_admin\_username=\"orsee_user/site\_\_database\_admin\_username=\"''$DBNAME''/' ../config/settings.php
-perl -i -p -e 's/site\_\_database\_admin\_password=\"orsee\_pw/site\_\_database\_admin\_password=\"''$DBPASSWD''/' ../config/settings.php
+perl -i -p -e 's/site\_\_database\_database=\"orsee\_db/site\_\_database\_database=\"'$DBNAME'/' ../config/settings.php
+perl -i -p -e 's/site\_\_database\_admin\_username=\"orsee_user/site\_\_database\_admin\_username=\"'$DBNAME'/' ../config/settings.php
+perl -i -p -e 's/site\_\_database\_admin\_password=\"orsee\_pw/site\_\_database\_admin\_password=\"'$DBPASSWD'/' ../config/settings.php
 perl -i -p -e 's/date\_default\_timezone\_set\(\"Australia\/Sydney/date\_default\_timezone\_set\(\"Europe\/Berlin/' ../config/settings.php
 
 perl -i -p -e 's/ORSEEROOT=\/srv\/www\/htdocs\/orsee/ORSEEROOT=\/var\/www\/html\/pesa/' ../install/crontab-for-orsee
