7ee4bdd8cfb628e12477c2f09a8a2877b8ea00bf
root <root@pesa.psychologie.hu-berlin.de>
2020-06-20 05:57:46 +0200

    adapted header for our institute

--- style/orsee/html_header.php
+++ style/orsee/html_header.php
@@ -1,21 +1,20 @@
 <basefont face="Arial,Helvetica,sans-serif">
 
 <center>
-<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100%">
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="2" WIDTH="100%">
     <TR VALIGN="TOP" ALIGN="LEFT" BGCOLOR="#html_header_top_bar_background#">
         <TD WIDTH="30%" ALIGN="LEFT" VALIGN="MIDDLE">
-            <IMG border="0" SRC="../style/orsee/orsee3_sign.png">
+            <IMG border="0" SRC="../style/orsee/psi.png">
         </TD>
         <TD WIDTH="70%" BGCOLOR="#html_header_top_bar_background#">
-            <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="100%">
-            <TR><TD HEIGHT="19" ALIGN="RIGHT" VALIGN="BOTTOM" bgcolor="#html_header_logo_bar_background#">
-                <P><FONT face="Arial" size="1" class="small" COLOR="#FFFFFF">ONLINE RECRUITMENT SYSTEM FOR ECONOMIC EXPERIMENTS</FONT>
+            <TABLE BORDER="0" CELLSPACING="0" CELLPADDING="5" WIDTH="100%">
+            <TR><TD HEIGHT="19" ALIGN="CENTER" VALIGN="BOTTOM" bgcolor="#html_header_logo_bar_background#">
+                <P><FONT face="Arial" size="5" COLOR="#DDDDDD">Humboldt-Universit&auml;t zu Berlin &ndash; Institut f&uuml;r Psychologie</FONT>
             </TD></TR>
-            <TR><TD VALIGN="MIDDLE" ALIGN="RIGHT" style="border-radius: 0px 0px 0px 20px; background: #html_header_logo_bar_background#;">
-                    <IMG SRC="../style/orsee/orsee3_logo.png" BORDER="0">&nbsp;
-            </TD></TR>
-            <TR><TD HEIGHT="19" valign="middle" align="right">
-                <FONT face="Arial" size="1" class="small" COLOR="#FFFFFF">THE EASE OF RECRUITMENT</FONT>
+            <TR><TD VALIGN="MIDDLE" ALIGN="CENTER" style="border-radius: 0px 0px 0px 20px; background: #html_header_logo_bar_background#;">
+                 <P><FONT face="Arial" size="7" COLOR="#FFFFFF">P</FONT><FONT face="Arial" size="6" class="large" COLOR="#DDDDDD">sychologischer</FONT>
+                    <FONT face="Arial" size="7" class="large" COLOR="#FFFFFF">E</FONT><FONT face="Arial" size="6" class="large" COLOR="#DDDDDD">xperimental-</FONT><FONT face="Arial" size="7" class="large" COLOR="#FFFFFF">S</FONT><FONT face="Arial" size="6" class="large" COLOR="#DDDDDD">erver</FONT>
+                    <FONT face="Arial" size="7" class="large" COLOR="#FFFFFF">A</FONT><FONT face="Arial" size="6" class="large" COLOR="#DDDDDD">dlershof</FONT>
             </TD></TR>
             </TABLE>
         </TD>
